343962cd313ae9bfbc1d9890a3906694f8a1e7bb
Matthew Gamble <matthew.gamble@gmail.com>
2015-03-17 13:52:51 +0000

    Refactor ucf to use zipext module

--- ucf.py
+++ ucf.py
@@ -5,7 +5,7 @@ import datetime
 import tempfile
 import binascii
 
-from zipfile import ZipFile
+from zipext import ZipFileExt
 
 META_INF_DIR = "META-INF"
 MIMETYPE_FILE = "mimetype"
@@ -23,99 +23,6 @@ DEFAULT_RESERVED_FILES = [ MIMETYPE_FILE,
                    ]
 
 DEFAULT_RESERVED_DIRECTORIES = [ META_INF_DIR ]
-
-
-class ZipFileExt(ZipFile):
-
-    def __init__(self, file, mode="r", compression=zipfile.ZIP_STORED, allowZip64=True):
-        print("ZipFileExt called")
-        super().__init__(file,mode=mode,compression=compression,allowZip64=allowZip64)
-        self.requires_commit = False
-
-    def remove(self, zinfo_or_arcname):
-        if not self.fp:
-            raise RuntimeError(
-                "Attempt to modify to ZIP archive that was already closed")
-
-        if isinstance(zinfo_or_arcname, zipfile.ZipInfo):
-            zinfo = zinfo_or_arcname
-        else:
-            zinfo = self.getinfo(zinfo_or_arcname)
-
-        self.filelist.remove(zinfo)
-        self.requires_commit = True
-
-    def rename(self, zinfo_or_arcname, filename):
-        if not self.fp:
-            raise RuntimeError(
-                "Attempt to modify to ZIP archive that was already closed")
-
-        # Terminate the file name at the first null byte.  Null bytes in file
-        # names are used as tricks by viruses in archives.
-        null_byte = filename.find(chr(0))
-        if null_byte >= 0:
-            filename = filename[0:null_byte]
-        # This is used to ensure paths in generated ZIP files always use
-        # forward slashes as the directory separator, as required by the
-        # ZIP format specification.
-        if os.sep != "/" and os.sep in filename:
-            filename = filename.replace(os.sep, "/")
-
-        if isinstance(zinfo_or_arcname, zipfile.ZipInfo):
-            zinfo = zinfo_or_arcname
-        else:
-            zinfo = self.getinfo(zinfo_or_arcname)
-
-        zinfo.filename = filename
-        self.requires_commit = True
-
-
-    def close(self):
-        """Close the file, and for mode "w" and "a" write the ending
-        records."""
-        if self.fp is None:
-            return
-
-        try:
-            if self.mode in ("w", "a") and self._didModify: # write ending records
-
-                if self.requires_commit:
-                    self.commit()
-
-                with self._lock:
-                    try:
-                        self.fp.seek(self.start_dir)
-                    except (AttributeError, io.UnsupportedOperation):
-                        # Some file-like objects can provide tell() but not seek()
-                        pass
-                    self._write_end_record()
-        finally:
-            fp = self.fp
-            self.fp = None
-            self._fpclose(fp)
-
-    def commit(self):
-
-        with ZipFile(tempfile.NamedTemporaryFile(delete=False),mode="w") as new_zip:
-            for name in self.namelist():
-                bytes = self.read(name)
-                new_zip.writestr(name,bytes)
-            badfile = new_zip.testzip()
-        if(badfile):
-            raise zipfile.BadZipFile("Error when writing updated zipfile, failed zipfile CRC-32 check: file is corrupt")
-        else:
-            #mv self.filename to old, new to self.filename, and then remove old
-            #TODO check: The filenames here should always be absolute?
-            old = tempfile.NamedTemporaryFile(delete=False)
-            old.close()
-            os.rename(self.filename,old.name)
-            os.rename(new_zip.filename,self.filename)
-            #TODO instead of reusing __init__ it would be nicer to establish
-            #what really needs doing to reset. This would however likely result
-            #in code duplication from zipfile.ZipFile's init method.
-            #Really we need a reset function in zipfile.
-            self.__init__(file=self.filename,mode='r',compression=self.compression,allowZip64=self._allowZip64,mimetype=self.mimetype)
-
 
 
 class UCF(ZipFileExt):
@@ -206,7 +113,7 @@ class UCF(ZipFileExt):
     @classmethod
     def from_zipfile(cls,file, compression=zipfile.ZIP_STORED, allowZip64=True,mimetype=None):
         """
-        Creates a new UCF file from an existing valid zipfile
+        Creates a new UCF file from an existing valid zipfile.
         """
         pass
 
@@ -305,9 +212,6 @@ class UnsupportedCompressionException(UCFException):
     pass
 
 
-
-
-
 def print_info(archive_name):
     zf = zipfile.ZipFile(archive_name)
     for info in zf.infolist():
@@ -381,13 +285,9 @@ def main():
         if 'junk' not in container.namelist():
             container.writestr('junk','some junk text')
 
-    #    if 'mimetype' not in container.namelist():
-    #        container._add_mimetype_file()
-    #        print("added mimetype")
-
-        #print_info(filename)
-        for name in container.namelist(ignore_reserved=True):
-            print(name)
+        if 'mimetype' not in container.namelist():
+            container._add_mimetype_file()
+            print("added mimetype")
 
         for file in container.infolist():
             print(file.filename)
