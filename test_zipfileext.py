c5e0c1b212c403c7b0c07027d6af752f0e195def
Matthew Gamble <matthew.gamble@gmail.com>
2015-03-19 16:40:38 +0000

    Make tests more verbose with supporting messages

--- test_zipfileext.py
+++ test_zipfileext.py
@@ -47,6 +47,7 @@ class AbstractZipExtTestWithSourceFile:
         self.make_test_archive(f,compression)
 
         with zipext.ZipFileExt(f, "a", compression) as zipfp:
+
             self.assertEqual(zipfp.read(TESTFN), self.data)
             self.assertEqual(zipfp.read("another.name"), self.data)
             self.assertEqual(zipfp.read("strfile"), self.data)
@@ -75,33 +76,35 @@ class AbstractZipExtTestWithSourceFile:
                 self.assertEqual(i.file_size, len(self.data))
 
         with zipext.ZipFileExt(f, "r", compression) as zipfp:
+
             #Check remaining data
-            self.assertEqual(zipfp.read("another.name"), self.data)
-            self.assertEqual(zipfp.read("strfile"), self.data)
+            self.assertEqual(zipfp.read("another.name"), self.data, "Error reading file: another.name")
+            self.assertEqual(zipfp.read("strfile"), self.data,"Error reading file: strfile")
+
             # Check the namelist
             names = zipfp.namelist()
-            self.assertEqual(len(names), 2)
+            self.assertEqual(len(names), 2, "Incorrect number of files in archive namelist - archive has {:d} entries, should be 2".format(len(names)))
             # Check present files
-            self.assertIn("another.name", names)
-            self.assertIn("strfile", names)
+            self.assertIn("another.name", names, "File another.name is missing from archive namelist")
+            self.assertIn("strfile", names,"File strfile is missing from archive namelist")
             # Check removed file
-            self.assertNotIn(TESTFN, names)
+            self.assertNotIn(TESTFN, names,"File {:s} should not be present in namelist".format(TESTFN))
 
             # Check infolist
             infos = zipfp.infolist()
             names = [i.filename for i in infos]
-            self.assertEqual(len(names), 2)
-            self.assertIn("another.name", names)
-            self.assertIn("strfile", names)
-            self.assertNotIn(TESTFN, names)
+            self.assertEqual(len(names),2,"Incorrect number of files in archive infolist - archive has {:d} entries, should be 2".format(len(names)))
+            self.assertIn("another.name", names,"File another.name is missing from archive infolist")
+            self.assertIn("strfile", names,"File strfile is missing from archive infolist")
+            self.assertNotIn(TESTFN, names,"File {:s} should not be present in infolist".format(TESTFN))
             for i in infos:
-                self.assertEqual(i.file_size, len(self.data))
+                self.assertEqual(i.file_size, len(self.data), "Filesize {:d} is not equal to expected: {:d}".format(i.file_size,len(self.data)))
 
             # check getinfo
             for nm in ("another.name", "strfile"):
                 info = zipfp.getinfo(nm)
-                self.assertEqual(info.filename, nm)
-                self.assertEqual(info.file_size, len(self.data))
+                self.assertEqual(info.filename, nm,"Filename {:s} is not equal to expected name: {:s}".format(info.filename,nm))
+                self.assertEqual(info.file_size, len(self.data),"Filesize {:d} is not equal to expected: {:d}".format(i.file_size,len(self.data)))
 
             # Check that testzip doesn't raise an exception
             zipfp.testzip()
@@ -125,13 +128,10 @@ class AbstractZipExtTestWithSourceFile:
             self.assertEqual(zipfp.read("another.name"), self.data)
             self.assertEqual(zipfp.read("strfile"), self.data)
             TESTFN_NEW = ''.join(["new",TESTFN])
-            print(TESTFN_NEW)
             zipfp.rename(TESTFN,TESTFN_NEW)
 
             # Check the namelist
             names = zipfp.namelist()
-            for n in names:
-                print(n)
             self.assertEqual(len(names), 3)
             #Check renamed file
             self.assertIn(TESTFN_NEW, names)
