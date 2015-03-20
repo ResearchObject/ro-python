3acce2ede3e5205c559af55815c344eb74a92d9f
Matthew Gamble <matthew.gamble@gmail.com>
2015-03-20 15:41:45 +0000

    Use support functions from tests.support

--- tests/test_zipfileext.py
+++ tests/test_zipfileext.py
@@ -9,27 +9,7 @@ from tempfile import NamedTemporaryFile
 import io
 import os
 
-TESTFN = '@test'
-TESTFN = "{}_{}_tmp".format(TESTFN, os.getpid())
-
-TESTFN2 = TESTFN + "2"
-
-def unlink(filename):
-    try:
-        os.unlink(filename)
-    except (FileNotFoundError, NotADirectoryError):
-        pass
-
-
-def get_files(test):
-    yield TESTFN2
-    with TemporaryFile() as f:
-        yield f
-        test.assertFalse(f.closed)
-    with io.BytesIO() as f:
-        yield f
-        test.assertFalse(f.closed)
-
+from tests.support import (TESTFN, TESTFN2, unlink, get_files)
 
 class AbstractZipExtTestWithSourceFile:
 
